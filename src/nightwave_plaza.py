ee3b2a29076c5b2512a2704e787c36e7678ccbf2
huok <77536370+vraestoren@users.noreply.github.com>
2026-04-13 13:31:32 +0300

    Update nightwave_plaza.py

--- src/nightwave_plaza.py
+++ src/nightwave_plaza.py
@@ -1,129 +1,128 @@
 from requests import Session
 
 class NightWavePlaza:
-    def __init__(self) -> None:
-        self.api = "https://api.plaza.one"
-        self.session = Session()
-        self.session.headers = {
-            "User-Agent": "NightwavePlaza/1.4.1 (Android: SM-G9880; samsung z3q; ru)",
-            "Np-User-Agent": "Nightwave Plaza Axios"
-        }
+	def __init__(self) -> None:
+		self.api = "https://api.plaza.one"
+		self.session = Session()
+		self.session.headers = {
+			"User-Agent": "NightwavePlaza/1.4.1 (Android: SM-G9880; samsung z3q; ru)",
+			"Np-User-Agent": "Nightwave Plaza Axios"
+		}
 
-    def get_status(self) -> dict:
-        return self.session.get(f"{self.api}/status").json()
+	def _get(self, endpoint: str, params: dict = {}) -> dict:
+		return self.session.get(
+			f"{self.api}{endpoint}", params=params).json()
 
-    def get_random_background(self) -> dict:
-        return self.session.get(
-            f"{self.api}/backgrounds/random").json()
 
-    def get_captcha(self) -> dict:
-        return self.session.get(f"{self.api}/captcha").json()
+	def _post(self, endpoint: str, data: dict = None) -> dict:
+		return self.session.get(
+			f"{self.api}{endpoint}", data=data).json()
 
-    def login(self, username: str, password: str) -> dict:
-        data = {
-            "username": username,
-            "password": password
-        }
-        response = self.session.post(
-            f"{self.api}/user/auth", data=data).json()
-        if "token" in response:
-            self.token = response["token"]
-            self.session.headers["Authorization"] = f"Bearer {self.token}"
-        return response
+	def _put(self, endpoint: str, data: dict = None) -> dict:
+		return self.session.get(
+			f"{self.api}{endpoint}", data=data).json()
 
-    def register(
-            self,
-            email: str,
-            captcha: str,
-            username: str,
-            password: str,
-            captcha_key: str) -> dict:
-        data = {
-            "captcha": captcha,
-            "email": email,
-            "key": captcha_key,
-            "password": password,
-            "username": username
-        }
-        return self.session.post(
-            f"{self.api}/user/register", data=data).json()
+	def get_status(self) -> dict:
+		return self._get("/status")
 
-    def set_reaction(self, reaction: int) -> dict:
-        data = {
-            "reaction": reaction
-        }
-        return self.session.post(
-            f"{self.api}/reactions", data=data).json()
+	def get_random_background(self) -> dict:
+		return self._get("/backgrounds/random")
 
-    def get_backgrounds_list(self) -> dict:
-        return self.session.get(f"{self.api}/backgrounds").json()
+	def get_captcha(self) -> dict:
+		return self._get("/captcha")
 
-    def get_play_history(self, page: int = 1) -> dict:
-        return self.session.get(
-            f"{self.api}/history/{page}").json()
+	def login(self, username: str, password: str) -> dict:
+		data = {
+			"username": username,
+			"password": password
+		}
+		response = self._post("/user/auth", data)
+		if "token" in response:
+			self.token = response["token"]
+			self.session.headers["Authorization"] = f"Bearer {self.token}"
+		return response
 
-    def get_ratings(self, type: str = "overtime", page: int = 1) -> dict:
-        return self.session.get(
-            f"{self.api}/ratings/{type}/{page}").json()
+	def register(
+			self,
+			email: str,
+			captcha: str,
+			username: str,
+			password: str,
+			captcha_key: str) -> dict:
+		data = {
+			"captcha": captcha,
+			"email": email,
+			"key": captcha_key,
+			"password": password,
+			"username": username
+		}
+		return self._post("/user/register", data=data)
 
-    def get_news(self, page: int = 1) -> dict:
-        return self.session.get(
-            f"{self.api}/news/{page}").json()
+	def set_reaction(self, reaction: int) -> dict:
+		data = {
+			"reaction": reaction
+		}
+		return self._post("/reactions", data=data)
 
-    def get_song_favorites(self, page: int = 1) -> dict:
-        return self.session.get(
-            f"{self.api}/user/favorites/{page}").json()
+	def get_backgrounds_list(self) -> dict:
+		return self._get("/backgrounds")
 
-    def change_password(
-            self,
-            current_password: str,
-            new_password: str) -> dict:
-        data = {
-            "current_password": current_password,
-            "password": new_password
-        }
-        return self.session.put(
-            f"{self.api}/user", data=data).json()
+	def get_play_history(self, page: int = 1) -> dict:
+		return self._get(f"/history/{page}")
 
-    def change_email(self, email: str, password: str) -> dict:
-        data = {
-            "current_password": password,
-            "email": email
-        }
-        return self.session.put(
-            f"{self.api}/user", data=data).json()
+	def get_ratings(
+			self, type: str = "overtime", page: int = 1) -> dict:
+		return self._get(f"/ratings/{type}/{page}")
 
-    def reset_password(
-            self,
-            email: str,
-            captcha: str,
-            captcha_key: str) -> dict:
-        data = {
-            "captcha": captcha,
-            "email": email,
-            "key": captcha_key
-        }
-        return self.session.post(
-            f"{self.api}/user/reset", data=data).json()
+	def get_news(self, page: int = 1) -> dict:
+		return self._get(f"/news/{page}")
 
-    def get_song_info(self, song_id: str) -> dict:
-        return self.session.get(
-            f"{self.api}/songs/{song_id}").json()
+	def get_song_favorites(self, page: int = 1) -> dict:
+		return self._get(f"/user/favorites/{page}")
 
-    def get_account_info(self) -> dict:
-        return self.session.get(f"{self.api}/user").json()
+	def change_password(
+			self,
+			current_password: str,
+			new_password: str) -> dict:
+		data = {
+			"current_password": current_password,
+			"password": new_password
+		}
+		return self._put("/user", data)
 
-    def add_song_to_favorites(self, song_id: str) -> dict:
-        data = {
-            "song_id": song_id
-        }
-        return self.session.post(
-            f"{self.api}/user/favorites", data=data).json()
-    
-    def get_on_screen_data(self) -> dict:
-        return self.session.get(
-            f"{self.api}/status/on-screen-data").json()
-    
-    def get_background_info(self, background_id: str) -> dict:
-        return self.session.get(
-            f"{self.api}/backgrounds/{background_id}").json()
+	def change_email(self, email: str, password: str) -> dict:
+		data = {
+			"current_password": password,
+			"email": email
+		}
+		return self._put("/user", data)
+
+	def reset_password(
+			self,
+			email: str,
+			captcha: str,
+			captcha_key: str) -> dict:
+		data = {
+			"captcha": captcha,
+			"email": email,
+			"key": captcha_key
+		}
+		return self._post("/user/reset", data)
+
+	def get_song_info(self, song_id: str) -> dict:
+		return self._get(f"/songs/{song_id}")
+
+	def get_account_info(self) -> dict:
+		return self._get("/user")
+
+	def add_song_to_favorites(self, song_id: str) -> dict:
+		data = {
+			"song_id": song_id
+		}
+		return self._post("/user/favorites", data)
+	
+	def get_on_screen_data(self) -> dict:
+		return self._get("/status/on-screen-data")
+	
+	def get_background_info(self, background_id: str) -> dict:
+		return self._get(f"/backgrounds/{background_id}")
